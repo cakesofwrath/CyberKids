0c0b20dfc20f9ce4752f44623fa76ffab81a3e47
cakesofwrath <dai.alan.dai@gmail.com>
2015-05-27 22:59:23 -0400

    header, footer

---
+++ app/js/controllers/LearnCtrl.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var LearnCtrl = function($scope) {
+    $scope.testVar = "Welcome!";  
+};
+
+module.exports = LearnCtrl;
--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,30 @@
 "use strict";
 
 var angular = require("angular"),
-    HomeCtrl = require("./controllers/HomeCtrl");
+    HomeCtrl = require("./controllers/HomeCtrl"),
+    LearnCtrl = require("./controllers/LearnCtrl");
 
-var app = angular.module("ScratchCourse", []);
+var app = angular.module("ScratchCourse", [require("angular-route")]);
 
-app.controller("HomeCtrl", ["$scope", HomeCtrl]);
+app.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "../views/pages/home.html",
+            controller: "HomeCtrl"
+        })
+        .when("/learn", {
+            templateUrl: "../views/pages/learn.html",
+            controller: "LearnCtrl"
+        })
+        .otherwise({
+            //should 404 later
+            redirectTo: "/" // can do this with others too
+        });
+        // https://scotch.io/tutorials/single-page-apps-with-angularjs-routing-and-templating
+    $locationProvider.html5Mode(true); //gets rid of hash in url
+});
+
+app.controller("HomeCtrl", ["$scope", HomeCtrl])
+    .controller("LearnCtrl", ["$scope", LearnCtrl]);
+//https://docs.angularjs.org/api/ngRoute/service/$route#example
+// app.controller("HomeCtrl", ["$scope", HomeCtrl]);
